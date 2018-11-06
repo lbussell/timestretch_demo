ab909c47819f1a16233d0332a82ca23ad3f62861
Logan Bussell <lsbussell@gmail.com>
2018-11-06 13:34:10 -0500

    implement drag & drop and tempo slider

--- test.js
+++ test.js
@@ -8,20 +8,43 @@ stretch.tempo = 1;
 var context = new AudioContext();
 var buffer;
 
-function loadSample(url) {
-    var request = new XMLHttpRequest();
-    request.open('GET', url, true);
-    request.responseType = 'arraybuffer';
-    request.onload = function() {
-      context.decodeAudioData(request.response, function(data) {
-        buffer = data;
-        play();
-      })
-    }
-    request.send();
+var slide = document.getElementById('slider'),
+    sliderDiv = document.getElementById("sliderValue");
+
+function dropHandler(ev) {
+    console.log('File(s) dropped');
+  
+    // Prevent default behavior (Prevent file from being opened)
+    ev.preventDefault();
+  
+    if (ev.dataTransfer.items) {
+        // Use DataTransferItemList interface to access the file(s)
+        var files = ev.dataTransfer.files
+        for (var i = 0; i < files.length; i++) {
+            var file = files[i]
+            var reader = new FileReader()
+            reader.addEventListener('load', function(ev) {
+                loadSample(ev.target.result);
+            })
+            reader.readAsArrayBuffer(file)
+        }
+    } else {
+        // Use DataTransfer interface to access the file(s)
+        for (var i = 0; i < ev.dataTransfer.files.length; i++) {
+            console.log('... file[' + i + '].name = ' + ev.dataTransfer.files[i].name);
+        }
+    } 
+    
+    // Pass event to removeDragData for cleanup
+    removeDragData(ev)
 }
 
-loadSample('track.mp3')
+function loadSample(data) {
+    context.decodeAudioData(data, function(fileBuffer) {
+        buffer = fileBuffer;
+        play();
+    });
+}
 
 var BUFFER_SIZE = 1024;
 var samples = new Float32Array(BUFFER_SIZE * 2);
@@ -46,6 +69,30 @@ function play() {
 
 function pause() {
     node.disconnect();
+}
+
+function removeDragData(ev) {
+    console.log('Removing drag data');
+  
+    if (ev.dataTransfer.items) {
+        // Use DataTransferItemList interface to remove the drag data
+        ev.dataTransfer.items.clear();
+    } else {
+        // Use DataTransfer interface to remove the drag data
+        ev.dataTransfer.clearData();
+    }
+}
+
+function dragOverHandler(ev) {
+    console.log('File(s) in drop zone'); 
+  
+    // Prevent default behavior (Prevent file from being opened)
+    ev.preventDefault();
+}
+
+slide.onchange = function() {
+    stretch.tempo = this.value;
+    sliderDiv.innerHTML = this.value;
 }
 
 var source = {
